97aaef61041b3adb7b10e4e12b5a0da6bcb830d9
Praveen Sonare <praveen@scooterson.com>
2023-10-03 15:03:34 +0800

    public and authorized api

--- exec.sh
+++ exec.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+#docker run --name db_telemetry -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=telemetry -d postgres
 docker build -t telemetry .
 docker run -p 9000:9000 -it telemetry
